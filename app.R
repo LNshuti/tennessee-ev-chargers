f388204a5062de6cd174bb16ea04377a66cda3b1
LNshuti <nshutl0@sewanee.edu>
2022-04-13 21:52:33 -0500

    update shiny

--- app.R
+++ app.R
@@ -1,27 +1,38 @@
 library(shiny) 
 library(leaflet) 
-  
-r_colors <- rgb(t(col2rgb(colors()) / 255)) 
-names(r_colors) <- colors() 
-      
-ui <- fluidPage(leafletOutput("mymap"), p(), 
-                actionButton("recalc", "New points")) 
+library(tigris)
+library(leaflegend)
+library(htmltools)
+library(RColorBrewer)
+
+cb_tn <- core_based_statistical_areas(year = 2018, cb = TRUE)
+davidson.metro <- filter(cb_tn, grepl("Davidson", NAME))
+
+n <- 9
+qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual',]
+col_vector = unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
+
+pal <- colorFactor(palette = col_vector, levels = ev_stations_davidson_$charger_type)
+ 
+ev_stations_davidson_ <- 
+  read_rds(paste0(repo_path, "/data/shiny/ev_stations_davidson_.rds")) 
+
+ui <- fluidPage(leafletOutput("mymap"), p()) 
         
 server <- function(input, output, session) { 
-  points <- eventReactive(input$recalc,
-                          {cbind(rnorm(40) * 2 + 13, rnorm(40) + 48) 
-                   },
-                   ignoreNULL = FALSE) 
-              
-  output$mymap <- renderLeaflet({ 
-    leaflet() %>% 
-      addProviderTiles(providers$Stamen.TonerLite, 
-                       options = providerTileOptions(noWrap = TRUE)) %>% 
-                         addMarkers(data = points()) 
-                     }) 
-                 } 
+
+output$mymap <- renderLeaflet({ 
+  leaflet(davidson.metro) %>% 
+    addTiles() %>% 
+    addMarkers(data = ev_stations_davidson_,
+               ~longitude, ~latitude, popup = ~htmlEscape(charger_type),
+               labelOptions = labelOptions(noHide = T, textOnly = FALSE)
+               )
+                                     
+                     }
+    )
+  
+}
+                 
           
 shinyApp(ui, server) 
-           
-          
-          
