8deffc0e7e919e35f8a0b92b1832237f9f1238cd
LNshuti <nshutl0@sewanee.edu>
2022-05-02 15:08:47 -0500

    update ui

--- app.R
+++ app.R
@@ -1,37 +1,97 @@
 library(shiny) 
+library(shinydashboard)
+library(DT)
 library(leaflet) 
 library(tigris)
 library(leaflegend)
 library(htmltools)
 library(RColorBrewer)
+library(data.table)
+library(tidyverse)
 
-cb_tn <- core_based_statistical_areas(year = 2018, cb = TRUE)
-davidson.metro <- filter(cb_tn, grepl("Davidson", NAME))
+set.seed(1234)
 
-n <- 9
-qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual',]
-col_vector = unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
+# cb_tn <- core_based_statistical_areas(year = 2018, cb = TRUE)
+# davidson.metro <- filter(cb_tn, grepl("Davidson", NAME))
 
-pal <- colorFactor(palette = col_vector, levels = ev_stations_davidson_$charger_type)
- 
-ev_stations_davidson_ <- 
-  read_rds(paste0(repo_path, "/data/shiny/ev_stations_davidson_.rds")) 
+load("data/shiny/ev_stations_davidson_.rda")
+load("data/shiny/davidson.metro.rda")
 
-ui <- fluidPage(leafletOutput("mymap"), p()) 
+# n <- 9
+# factorPal <- colorFactor('Set1', ev_stations_davidson_$charger_type)
+
+getColor <-
+  function(ev_stations_davidson_) {
+    sapply(ev_stations_davidson_$charger_type, function(charger_type) {
+      if(charger_type == "Airport"){"darkgreen"}
+      else if(charger_type == "Apartment complex"){"gray"}
+      else if(charger_type == "Car dealer"){"green"}
+      else if(charger_type == "Grocery store"){"orange"}
+      else if(charger_type == "Hospital"){"red"}
+      else if(charger_type == "Metro"){"chocolate"}
+      else if(charger_type == "Tesla"){"azure"}
+      else if(charger_type == "Paid Parking"){"cornflowerblue"}
+      else {"darkred"}
+    })
+  }
+
+# 9 possible colors
+pal <- c("red", "darkred", "lightred", "orange", "beige",
+         "green", "darkgreen", "lightgreen","darkblue")
+
+icons <- 
+  awesomeIcons(
+    icon = "ios-close", 
+    lib = "ion",
+    iconColor = "#FFFFFF",
+    markerColor = pal[1:9]
+    )
+
+header<-dashboardHeader(title="Find EV Chargers Tennesse")
+
+body<-dashboardBody(
+  fluidRow(
+    column(width = 9,
+           box(width = NULL, solidHeader = TRUE,
+               leafletOutput("londonMap", height=400)
+           ),
+           box(width=NULL,
+               dataTableOutput("boroughTable")
+           )
+    ),
+    column(width=3,
+           box(width=NULL, 
+               uiOutput("yearSelect"),
+               radioButtons("meas", "Measure",c("Mean"="Mean", "Median"="Median")),
+               checkboxInput("city", "Include City of London?",TRUE)
+               
+           )
+    )
+  )
+)
+
+dashboardPage(
+  header,
+  dashboardSidebar(disable = TRUE),
+  body
+)
+
+ui <- fluidPage(leafletOutput("mymap"), tags$head(includeHTML(("google-analytics.html"))), p()) 
         
 server <- function(input, output, session) { 
-
+  
   output$mymap <- renderLeaflet({ 
     leaflet(davidson.metro) %>% 
       addTiles() %>% 
-      addMarkers(data = ev_stations_davidson_,
-                 ~longitude, ~latitude, popup = ~htmlEscape(charger_type),
-                 labelOptions = labelOptions(noHide = T, textOnly = FALSE)
-                 )
-                                       
-                       }
+      addAwesomeMarkers(data = ev_stations_davidson_, ~longitude, ~latitude, 
+                        icon = icons,
+                        #label = ~charger_type,
+                        popup = ~paste(street_address, "<br>",
+                                       station_name, "<br>",
+                                       "Number of Outlets = ", num_outlets)
+                        ) 
+    }
       )
-  
 }
-          
+
 shinyApp(ui, server) 
